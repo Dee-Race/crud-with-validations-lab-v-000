eba82f65ec94afe4c76a1cd8438c3515233e272b
Dee Race <deerace12@gmail.com>
2018-06-18 17:27:45 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: {scope: [:artist_name, :release_year]}
   validates :released, inclustion: {in: [true, false]}
-  validates :artist_name, presence: true 
+  validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released
 
 end
